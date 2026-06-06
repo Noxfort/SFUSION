87b54d0569041bc4fad47595175c9db01a02f61a
Noxfort <gmateusmoraes@gmail.com>
2026-06-06 13:40:26 -0300

    Remove unused test files and neural_transformer

--- test_pipeline.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-import pandas as pd
-from src.services.parquet_service import ParquetService
-
-df = pd.read_parquet('/home/gabriel-moraes/Documentos/DADOS/a.parquet')
-print("--- Parquet Output ---")
-print(df[['sensor_id', 'speed_val', 'flow_val', 'intensity_val']].head(24))
